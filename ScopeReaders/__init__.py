3d63b442b80b07c22014f1c3901977e3d1f9b4f2
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-30 00:07:28 -0400

    Populated skeleton structure for package

---
+++ ScopeReaders/__init__.py
@@ -0,0 +1,19 @@
+"""
+The ScopeReaders package
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: _autosummary
+
+"""
+from .__version__ import version as __version__
+
+
+__all__ = ['__version__']
+# Traditional hierarchical approach - importing submodules
+
+
+# Making things easier by surfacing all low-level modules directly:
+
