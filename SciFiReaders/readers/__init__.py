892e2432630e05d5be517b46e534aa8c49a4ac73
Gerd Duscher <50049264+gduscher@users.noreply.github.com>
2021-06-18 19:03:06 -0400

    added tests for hyperspy

--- SciFiReaders/readers/__init__.py
+++ SciFiReaders/readers/__init__.py
@@ -8,4 +8,5 @@ from .converters import *
 
 __all__ = microscopy.__all__ + generic.__all__ + SID.__all__ + converters.__all__ + spectroscopy.__all__
 
-all_readers = microscopy.all_readers + generic.all_readers + SID.all_readers + converters.all_readers + spectroscopy.all_readers
+all_readers = microscopy.all_readers + generic.all_readers + SID.all_readers + converters.all_readers + \
+              spectroscopy.all_readers
