03dfa2aee91fb9dc89c4da9f0675a2996f3dd808
Suhas Somnath <14300780+ssomnath@users.noreply.github.com>
2021-10-19 17:47:17 -0400

    Added try catch to make code work
    Also renamed translation to reading

--- SciFiReaders/ingestor.py
+++ SciFiReaders/ingestor.py
@@ -2,11 +2,9 @@
 Utilities for automated ingestion of data and metadata in proprietary file formats
 """
 
-from __future__ import division, unicode_literals, print_function, absolute_import
 from warnings import warn
-from . import readers
+from .readers import all_readers
 
-all_translators = readers.all_readers
 
 def ingest(file_path):
     """
@@ -21,20 +19,34 @@ def ingest(file_path):
     -------
     Translated file (output of translator.read())
     """
-    valid_translators = []
-    for translator in all_translators:
-        t = translator(file_path)
-        if t.can_read():
-            valid_translators.append(translator)
-    if len(valid_translators) == 0:
+    valid_readers = []
+    for this_reader in all_readers:
+        try:
+            t = this_reader(file_path)
+        except Exception:
+            continue
+        try:
+            # TODO: This code should be absorbed into the constructor. That can fail anyway
+            readable = t.can_read()
+        except Exception:
+            continue
+        if readable:
+            valid_readers.append(this_reader)
+            # print('{} is a valid reader'.format(this_reader))
+            
+    final_reader = valid_readers[0]
+    if len(valid_readers) == 0:
         raise TypeError(
-            "The automatic search for a suitable translator was unsuccessful")
-    elif len(valid_translators) > 1:
-        warn("Multiple translators may be able to read your file."
-             + "The {} will be applied.".format(valid_translators[-1])
+            "The automatic search for a suitable Reader was unsuccessful")
+    elif len(valid_readers) > 1:
+        warn("Multiple Reader s may be able to read your file."
+             + "The {} will be applied.".format(valid_readers[-1])
              + " Consider specifying 'force_translator'")
-        translated = valid_translators[-1](file_path).read()
-    else:
-        translated = valid_translators[0](file_path).read()
+        final_reader = valid_readers[-1]
+    
+    try:
+        extracted = final_reader(file_path).read()
+    except Exception:
+        return None
 
-    return translated
+    return extracted
