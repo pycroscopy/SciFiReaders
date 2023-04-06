d70f6dc038cd5875bd44a25ff4eef880afc7fcc3
Slautin <bslautin@gmail.com>
2023-04-06 11:24:21 +0500

    read_header added

--- SciFiReaders/readers/microscopy/spm/afm/mdt.py
+++ SciFiReaders/readers/microscopy/spm/afm/mdt.py
@@ -3,8 +3,80 @@
 import numpy as np
 import sidpy as sid
 from sidpy.sid import Reader
-from mdt_reader.MDTfile_new import MDTfile
+import io
+from struct import *
+from abc import ABC
 
+#decarator has been copied from the https://github.com/symartin/PyMDT
+class MDTBufferedReaderDecorator(object):
+    """
+        a decorator class that facilitate the sequential reading of a file.
+
+        The class will redirect al the standard file methods and add some methods to read and integer and float number
+        encoded on 8, 16, 32 or 64 bits
+    """
+
+    def __init__(self, file_):
+        self._file = file_
+
+    def shift_position(self, shift_bytes):
+        self._file.seek(shift_bytes, io.SEEK_CUR)
+
+    def read_uint8(self):
+        """Read a unsigned integer coded on 1 byte (a char)"""
+        return unpack("<B", self._file.read(1))[0]
+
+    def read_uint16(self):
+        """Read a unsigned integer coded on 2 byte (a short)"""
+        return unpack("<H", self._file.read(2))[0]
+
+    def read_uint32(self):
+        """Read a unsigned integer coded on 4 byte (a usual int or long int)"""
+        return unpack("<I", self._file.read(4))[0]
+
+    def read_uint64(self):
+        """Read a unsigned integer coded on 8 byte (a long long)"""
+        return unpack("<Q", self._file.read(8))[0]
+
+    def read_int8(self):
+        """Read a signed integer coded on 1 byte (a char)"""
+        return unpack("<b", self._file.read(1))[0]
+
+    def read_int16(self):
+        """Read a signed integer coded on 2 byte (a short)"""
+        return unpack("<h", self._file.read(2))[0]
+
+    def read_int32(self):
+        """Read a unsigned integer coded on 4 byte (a usual int or long int)"""
+        return unpack("<i", self._file.read(4))[0]
+
+    def read_int64(self):
+        """Read a unsigned integer coded on 8 byte (a long long)"""
+        return unpack("<q", self._file.read(8))[0]
+
+    def read_char(self):
+        """Read one character coded on 1 byte (a usual char)"""
+        return unpack("<c", self._file.read(1))[0]
+
+    def read_uchar(self):
+        """Read a unsigned integer coded on 1 byte (a char)
+            idem that read_uint8()
+        """
+        return int(unpack('<B', self._file.read(1))[0])
+
+    def read_double(self):
+        return float(unpack('<d', self._file.read(8))[0])
+
+    def read_float32(self):
+        """Read a signed float coded on 4 byte (a float)"""
+        return float(unpack('<f', self._file.read(4))[0])
+
+    def read_float64(self):
+        """Read a signed float coded on 8 byte (au double float)"""
+        return float(unpack('<d', self._file.read(8))[0])
+
+    def __getattr__(self, attr):
+        return getattr(self._file, attr)
 
 class MDTReader(Reader):
     """
@@ -17,7 +89,39 @@ class MDTReader(Reader):
         super().__init__(file_path, *args, **kwargs)
 
     def read(self, verbose=False):
-        pass #TODO
+        self._file = MDTBufferedReaderDecorator(open(self._input_file_path, mode='rb'))
+
+        self._read_header(verbose=verbose)
+        #TODO read frame
+
+        self._file.close()
+
+    def _read_header(self, verbose):
+        '''
+
+        Read of the header of MDT file contained information about a number of frames (self.nb_frame)
+        and file size (self._file_size)
+
+        '''
+        # magic header
+        self._file.shift_position(4)
+
+        # File frm_byte_size (w/o header)
+        self._file_size = self._file.read_uint32()
+
+        #  4 bytes reserved (??)
+        self._file.shift_position(4)
+
+        # last frame
+        self.nb_frame = self._file.read_uint16()
+
+        #  19 bytes reserved (??)
+        self._file.shift_position(19)
+
+        if verbose:
+            print(f'File size: {self._file_size}')
+            print(f'Number of frames: {self.nb_frame}')
+
 
     def can_read(self):
         """
@@ -28,3 +132,17 @@ class MDTReader(Reader):
         """
 
         return super(MDTReader, self).can_read(extension='mdt')
+
+
+class Frame(ABC):
+    '''
+    Abstract class for frame description in general
+    '''
+
+    pass
+
+class MDT_Scan(Frame):
+    pass
+
+class MDT_Spectroscopy(Frame):
+    pass
