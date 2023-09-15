c5a7c0fa26ebb9ad367aae15c2f4439541badf90
Slautin <bslautin@gmail.com>
2023-09-16 01:16:08 +0200

    add MDTReader to __init__.py

--- SciFiReaders/readers/microscopy/spm/afm/__init__.py
+++ SciFiReaders/readers/microscopy/spm/afm/__init__.py
@@ -2,6 +2,7 @@ from .igor_ibw import IgorIBWReader, IgorMatrixReader
 from .AR_hdf5  import ARhdf5Reader
 from .bruker_nano import BrukerAFMReader
 from .gwyddion import GwyddionReader
+from .mdt import MDTReader
 
-__all__ = ['IgorIBWReader', 'ARhdf5Reader', 'BrukerAFMReader', 'GwyddionReader', 'IgorMatrixReader']
-all_readers = [IgorIBWReader, ARhdf5Reader, BrukerAFMReader, GwyddionReader, IgorMatrixReader]
+__all__ = ['IgorIBWReader', 'ARhdf5Reader', 'BrukerAFMReader', 'GwyddionReader', 'IgorMatrixReader','MDTReader']
+all_readers = [IgorIBWReader, ARhdf5Reader, BrukerAFMReader, GwyddionReader, IgorMatrixReader, MDTReader]
