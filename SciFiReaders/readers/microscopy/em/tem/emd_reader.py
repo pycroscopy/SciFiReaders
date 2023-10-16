correct units on emd reader for recip. space

SciFiReaders/readers/microscopy/em/tem/emd_reader.py
@@ -253,11 +253,11 @@ class EMDReader(sidpy.Reader):
             # for diffraction patterns
             # if you have an image stack of diffraction patterns, may not work?
             if '1/' in self.metadata['BinaryResult']['PixelUnitX']:
-                self.datasets[-1].set_dimension(0, sidpy.Dimension(np.arange(self.data_array.shape[0]) / scale_x,
+                self.datasets[-1].set_dimension(0, sidpy.Dimension(np.arange(self.data_array.shape[0]) * (scale_x / 1e18),
                                                                    name='u', units='1/nm',
                                                                    quantity='reciprocal distance',
                                                                    dimension_type='reciprocal'))
-                self.datasets[-1].set_dimension(1, sidpy.Dimension(np.arange(self.data_array.shape[1]) / scale_y,
+                self.datasets[-1].set_dimension(1, sidpy.Dimension(np.arange(self.data_array.shape[1]) * (scale_y / 1e18),
                                                                    name='v', units='1/nm',
                                                                    quantity='reciprocal distance',
                                                                    dimension_type='reciprocal'))