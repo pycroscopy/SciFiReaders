fc4fe79ac65326c7f31f6b544147a8d8b4b72826
ramav87 <rama87@gmail.com>
2024-06-18 14:07:52 -0400

    Update setup.py
    removing version requirement for setuptools

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(here, 'SciFiReaders/__version__.py')) as f:
 
 # TODO: Move requirements to requirements.txt
 requirements = [  # basic
-                'setuptools==58.2.0',
+                'setuptools',
                 'numpy',
                 'toolz',  # dask installation failing without this
                 'cytoolz',  # dask installation failing without this
