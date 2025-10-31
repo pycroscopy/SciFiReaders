f6bd25712c9f4c35c7e344130a2d626644e9acd1
Rama Vasudevan <vasudevanrk@ornl.gov>
2025-10-31 11:46:25 -0400

    try this one

--- tests/readers/microscopy/spm/afm/test_gwy.py
+++ tests/readers/microscopy/spm/afm/test_gwy.py
@@ -7,7 +7,7 @@ try:
     import gwyfile
 except ImportError:
     import pip
-    pip.main(['install', 'gwyfile'])
+    pip.main(['install', 'git+https://github.com/pycroscopy/gwyfile.git'])
 root_path = "https://github.com/pycroscopy/SciFiDatasets/blob/main/data/microscopy/spm/afm/"
 
 gwyfile = pytest.importorskip("gwyfile", reason="gwyfile not installed")
