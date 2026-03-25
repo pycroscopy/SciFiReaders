a21ef5909099ed2d82f46ec357d58cfc3aa2f37d
Sireesiru <37292550+Sireesiru@users.noreply.github.com>
2026-03-25 14:28:29 -0400

    Add CZIReader to module exports and all_readers

--- SciFiReaders/readers/microscopy/em/tem/__init__.py
+++ SciFiReaders/readers/microscopy/em/tem/__init__.py
@@ -14,7 +14,8 @@ from .dm_reader import DMReader, DM3Reader
 from .nion_reader import NionReader
 from .emd_reader import EMDReader
 from .mrc_reader import MRCReader
+from .czi_reader import CZIReader
 
-__all__ = ['DMReader', 'DM3Reader', 'NionReader', 'EMDReader', 'MRCReader']
+__all__ = ['DMReader', 'DM3Reader', 'NionReader', 'EMDReader', 'MRCReader', 'CZIReader']
 
-all_readers = [DMReader, DM3Reader, NionReader, EMDReader, MRCReader]
+all_readers = [DMReader, DM3Reader, NionReader, EMDReader, MRCReader, CZIReader]
