062eafeec902d4fd76a5189bfd1eb5c38ed0b336
Sireesiru <37292550+Sireesiru@users.noreply.github.com>
2026-03-25 23:21:31 -0400

    Update __init__.py

--- SciFiReaders/readers/microscopy/em/__init__.py
+++ SciFiReaders/readers/microscopy/em/__init__.py
@@ -3,5 +3,7 @@ from . import tem
 from .sem import *
 from . import sem
 
+#__all__ = tem.__all__ + sem.__all__
+#all_readers = tem.all_readers
 __all__ = tem.__all__ + sem.__all__
-all_readers = tem.all_readers
+all_readers = tem.all_readers + sem.all_readers
