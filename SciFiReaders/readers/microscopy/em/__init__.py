fd3725700a3018bd4be14de3d569daef442c606d
Sireesiru <37292550+Sireesiru@users.noreply.github.com>
2026-03-25 23:22:05 -0400

    Uncomment __all__ and all_readers assignments

--- SciFiReaders/readers/microscopy/em/__init__.py
+++ SciFiReaders/readers/microscopy/em/__init__.py
@@ -3,7 +3,5 @@ from . import tem
 from .sem import *
 from . import sem
 
-#__all__ = tem.__all__ + sem.__all__
-#all_readers = tem.all_readers
 __all__ = tem.__all__ + sem.__all__
 all_readers = tem.all_readers + sem.all_readers
