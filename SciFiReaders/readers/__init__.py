829c58c8642e63f9e2e9ade306b7e3bc908948e4
Sireesiru <37292550+Sireesiru@users.noreply.github.com>
2026-03-25 23:46:18 -0400

    Refactor imports in __init__.py
    Added imports for spectroscopy and converters modules.

--- SciFiReaders/readers/__init__.py
+++ SciFiReaders/readers/__init__.py
@@ -1,7 +1,12 @@
-from . import microscopy, SID, generic
+from . import microscopy
+from . import generic
+from . import SID
+from . import spectroscopy
+from . import converters
+
 from .microscopy import *
-from .SID import *
 from .generic import *
+from .SID import *
 from .spectroscopy import *
 from .converters import *
 
@@ -9,5 +14,4 @@ __all__ = microscopy.__all__ + generic.__all__ + SID.__all__ + \
           spectroscopy.__all__ + converters.__all__
 
 all_readers = microscopy.all_readers + generic.all_readers + \
-              SID.all_readers + spectroscopy.all_readers   #+ \
-              # converters.all_readers
+              SID.all_readers + spectroscopy.all_readers
