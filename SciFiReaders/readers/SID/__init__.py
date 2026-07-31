6844b35f9da790c9f2f0c83e9287874c34a592df
Aidan Swanger <101485239+aswanger011@users.noreply.github.com>
2026-07-31 16:16:19 -0400

    Remove NSIDWriter from exports and all_readers (changed back)

--- SciFiReaders/readers/SID/__init__.py
+++ SciFiReaders/readers/SID/__init__.py
@@ -1,6 +1,5 @@
 from .Nsid_reader import NSIDReader
-from .Nsid_writer import NSIDWriter
 from .Usid_reader import Usid_reader
 
-__all__ = ['NSIDReader', 'Usid_reader', 'Nsid_writer']
-all_readers = [NSIDReader, NSIDWriter, Usid_reader,]
+__all__ = ['NSIDReader', 'Usid_reader']
+all_readers = [NSIDReader, Usid_reader]
